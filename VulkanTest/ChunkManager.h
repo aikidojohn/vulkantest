2750730e63e210067b67bb67af0143ecebd814a5
John Hite <aikidojohn@gmail.com>
2018-05-30 17:24:18 -0400

    trying to generate terrain - it's broken now

--- VulkanTest/ChunkManager.h
+++ VulkanTest/ChunkManager.h
@@ -16,14 +16,24 @@
 #include <functional>
 #include <cstdlib>
 #include <vector>
+#include <queue>
 #include <cstring>
 #include <set>
 #include <algorithm>
 #include <array>
 #include <chrono>
+#include <random>
+#include <cmath>
 #include "Model.h"
 
 namespace blok {
+
+	struct diamond_params {
+		int step;
+		int x;
+		int z;
+		int mag;
+	};
 
 	struct Vertex {
 		glm::vec3 pos;
@@ -65,11 +75,12 @@ namespace blok {
 			mActive = active;
 		}
 	};
-
+	
+	static const float CUBE_SIZE = 0.25f;
 	class Chunk {
 	public:
 		static const int CHUNK_SIZE = 16;
-		Mesh<Vertex> mesh;
+		//Mesh<Vertex> mesh;
 
 		Chunk() {
 			mBlocks = new Block**[CHUNK_SIZE];
@@ -100,11 +111,15 @@ namespace blok {
 			delete[] mBlocks;
 		}
 
-		void render() {
-			mesh.clear();
-			float cubeSize = 0.25f;
-			float xOffset = 0.25f;
-			float yOffset = 0.25f;
+		void setActive(int x, int y, int z, bool active) {
+			mBlocks[x][y][z].setActive(false);
+		}
+
+		void render(Mesh<Vertex> &mesh, float xOffset, float yOffset, float zOffset) {
+			float cubeSize = CUBE_SIZE;
+			//float xOffset = 0.0f; //0.25f;
+			//float yOffset = 0.0f; //0.25f;
+			//float zOffset = 0.0f;
 			for (int x = 0; x < CHUNK_SIZE; x++) {
 				for (int y = 0; y < CHUNK_SIZE; y++) {
 					for (int z = 0; z < CHUNK_SIZE; z++) {
@@ -133,7 +148,7 @@ namespace blok {
 							if (y < CHUNK_SIZE - 1)
 								renderBottom = !mBlocks[x][y+1][z].isActive();
 
-							renderBlock(x * -cubeSize + xOffset, y * -cubeSize + yOffset, z * -cubeSize, rand() % 2, cubeSize, renderLeft, renderFront, renderRight, renderBack, renderTop, renderBottom);
+							renderBlock(mesh, x * -cubeSize + xOffset, y * -cubeSize + yOffset, z * -cubeSize + zOffset, rand() % 2, cubeSize, renderLeft, renderFront, renderRight, renderBack, renderTop, renderBottom);
 						}
 					}
 				}
@@ -141,9 +156,9 @@ namespace blok {
 		}
 
 	private:
-		Block * ** mBlocks;
+		Block*** mBlocks;
 
-		void renderBlock(float x, float y, float z, float textureIndex, float w, bool renderLeft, bool renderFront, bool renderRight, bool renderBack, bool renderTop, bool renderBottom) {
+		void renderBlock(Mesh<Vertex> &mesh, float x, float y, float z, float textureIndex, float w, bool renderLeft, bool renderFront, bool renderRight, bool renderBack, bool renderTop, bool renderBottom) {
 			float x0 = x;
 			float y0 = y;
 			float x1 = x + w;
@@ -208,6 +223,141 @@ namespace blok {
 				mesh.addTriangle(v0, v1, v2);
 				mesh.addTriangle(v2, v3, v0);
 			}
+		}
+	};
+
+	class ChunkManager {
+	public:
+		Mesh<Vertex> mesh;
+		
+		ChunkManager() {
+			for (int i = 0; i < worldSize * worldSize; i++) {
+				activeList.push_back(new Chunk());
+			}
+		}
+
+		~ChunkManager() {
+			for (int i = 0; i < activeList.size(); i++) {
+				delete activeList[i];
+			}
+		}
+
+		void render() {
+			diamondSquare();
+			for (int i = 0; i < activeList.size(); i++) {
+				activeList[i]->render(mesh, (float)(i/ worldSize) * Chunk::CHUNK_SIZE * CUBE_SIZE, 0.0f, (float)(i % worldSize) * Chunk::CHUNK_SIZE * CUBE_SIZE);
+			}
+			std::cout << "Vertex Count: " << mesh.vertices.size() << ", Triangles: " << mesh.indices.size() / 3 << std::endl;
+		}
+
+	private:
+		int worldSize = 12;
+		bool updateRequired = true;
+		std::vector<Chunk*> activeList;
+		std::vector<Chunk*> loadList;
+		std::vector<Chunk*> unloadList;
+
+		/*
+		Generate terrian using diamond square algorithm
+		*/
+		void diamondSquare() {
+			std::random_device realRandom;
+			std::seed_seq seed{ realRandom(), realRandom(), realRandom(), realRandom(), realRandom(), realRandom(), realRandom(), realRandom() };
+			std::mt19937 engine(seed);
+			std::uniform_int_distribution<> rand(0, 16);
+			
+			int mag = 16;
+			int size = 9;
+			int steps = log2(9);
+
+			int heightMap[9][9];
+			memset(heightMap, 0, sizeof(heightMap[0][0]) * size * size);
+			heightMap[0][0] = rand(engine);
+			heightMap[0][size -1] = rand(engine);
+			heightMap[size -1][0] = rand(engine);
+			heightMap[size -1][size -1] = rand(engine);
+			std::queue<diamond_params> queue;
+			queue.push({ size, 0, 0, 16 });
+			while (queue.size() > 0) {
+				diamond_params p = queue.front();
+				queue.pop();
+				diamond(p.step, p.z, p.z, mag, heightMap);
+				int next = p.step / 2;
+				int nextMag = p.mag - (mag / steps) + 1;
+				if (next > 1) {
+					queue.push({ next + 1, p.x, p.z, nextMag });
+					queue.push({ next + 1, p.x + next, p.z, nextMag });
+					queue.push({ next + 1, p.x, p.z + next, nextMag });
+					queue.push({ next + 1, p.x + next, p.z + next, nextMag });
+				}
+			}
+
+			/*for (int cx = 0; cx < worldSize; cx++) {
+				for (int cz = 0; cz < worldSize; cz++) {
+					Chunk* chunk = activeList[cx * worldSize + cz];
+					for (int x = 0; x < Chunk::CHUNK_SIZE; x++) {
+						for (int z = 0; z < Chunk::CHUNK_SIZE; z++) {
+							int maxY = heightMap[cx*Chunk::CHUNK_SIZE + x][cz*Chunk::CHUNK_SIZE + z];
+							for (int y = maxY; y < Chunk::CHUNK_SIZE; y++) {
+								chunk->setActive(x, y, z, false);
+							}
+						}
+					}
+				}
+			}*/
+		}
+
+		void diamond(int step, int xOffset, int zOffset, int mag, int (&heightMap)[9][9]) {
+			int mid = step / 2;
+			int corner = step - 1;
+			int size = 9;
+
+			heightMap[xOffset + mid][zOffset + mid] = avg(
+				heightMap[xOffset][zOffset], 
+				heightMap[xOffset][zOffset + corner],
+				heightMap[xOffset + corner][zOffset],
+				heightMap[xOffset + corner][zOffset + corner]);
+
+			//Square step
+			int wrap = zOffset - mid < 0 ? zOffset - mid + size - 1 : zOffset - mid;
+			heightMap[xOffset + mid][zOffset] = avg(
+				heightMap[xOffset + mid][zOffset + mid],
+				heightMap[xOffset][zOffset],
+				heightMap[xOffset + corner][zOffset],
+				heightMap[xOffset + mid][wrap]
+			); 
+
+			wrap = (zOffset + corner + mid) % size;
+			heightMap[xOffset + mid][zOffset + corner] = avg(
+				heightMap[xOffset + mid][zOffset + mid],
+				heightMap[xOffset][zOffset + corner],
+				heightMap[xOffset + corner][zOffset + corner],
+				heightMap[xOffset + mid][wrap]
+			);
+
+			wrap = xOffset - mid < 0 ? xOffset - mid + size - 1 : xOffset - mid;
+			heightMap[xOffset][zOffset + mid] = avg(
+				heightMap[xOffset + mid][zOffset + mid],
+				heightMap[xOffset][zOffset],
+				heightMap[xOffset][zOffset + corner],
+				heightMap[wrap][zOffset + mid]
+			);
+
+			wrap = (xOffset + corner + mid) % size;
+			heightMap[xOffset + corner][zOffset + mid] = avg(
+				heightMap[xOffset + mid][zOffset + mid],
+				heightMap[xOffset + corner][zOffset],
+				heightMap[xOffset + corner][zOffset + corner],
+				heightMap[wrap][zOffset + mid]
+			);
+		}
+
+		int avg(int a, int b, int c, int d) {
+			return (int)std::round((float)(a + b + c + d) / 4.0f);
+		}
+
+		int avg(int a, int b, int c, int d, int e) {
+			return (int)std::round((float)(a + b + c + d + e) / 5.0f);
 		}
 	};
 }
---
+++ VulkanTest/MemoryManager.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <functional>
+#include <cstdlib>
+#include <vector>
+#include <cstring>
+#include <set>
+#include <algorithm>
+#include <array>
+#include <chrono>
+
+namespace blok {
+
+};
